ba379946b902472667338b2557a6e64a20089f64
https://github.com/IancuPetru2/-My-Spring-Project.git <petruiancu656@gmail.com>
2024-10-22 18:22:34 +0300

    second commit

---
+++ src/test/java/net/commercial/app/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.commercial.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
